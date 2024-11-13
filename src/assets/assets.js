406b654f9e03a53a78b8e03e4ffaffad503b5bc6
Asma khemiri <benbrahimsalma18@gmail.com>
2024-11-14 00:22:18 +0100

    add products to Catalogue folder

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,25 +1,49 @@
 import logo from './logo.png';
 
 import surchemise2 from './Catalogue/surchemise2.png';
+
 import pontalon1 from './Catalogue/pontalon.png';
 import pontalon2 from './Catalogue/pontalongris.png';
-import robenoir from './Catalogue/robenoir.png';
+import pantalon3 from  './Catalogue/pontalon3.png';
+import pantalon4 from  './Catalogue/pantalon4.png';
+
+
+import pull3 from './Catalogue/pull3.png';
+import pull4 from './Catalogue/pull4.png';
+import pull5 from './Catalogue/pull5.png';
+import pull6 from './Catalogue/pull6.png';
+import pull7 from './Catalogue/pull7.png';
+import pull8 from './Catalogue/pull8.png';
+import pull9 from './Catalogue/pull9.png';
+import pullr from './Catalogue/pullrosee.png';
 
 import pullOverF1 from './Populaire/pull_overF1.png';
 import pullOverF2 from './Populaire/pull_overF2.png';
 import pullOverH1 from './Populaire/pull_overH1.png';
 import pullOverH2 from './Populaire/pull_overH2.png';
 
+
 export const assets = {
     logo,
     surchemise2,
     pontalon1,
     pontalon2,
-    robenoir,
+    pantalon3,
+    pantalon4,
+    
+    
     pullOverF1,
     pullOverF2,
     pullOverH1,
     pullOverH2,
+    pullr,
+    pull3,
+    pull4,
+    pull5,
+    pull6,
+    pull7,
+    pull8,
+    pull9
 };
 
 export const products = [
@@ -35,7 +59,7 @@ export const products = [
         id: 2,
         img: pullOverH1,
         title: "PULL OVER",
-        category: "homme",
+        category: "Homme",
         type:"pull",
         price: 59.90 
     },
@@ -51,7 +75,7 @@ export const products = [
         id: 4,
         img: pullOverH2,
         title: "PULL OVER",
-        category: "Femme",
+        category: "Homme",
         type:"pull",
         price: 59.90 
     },
@@ -66,6 +90,105 @@ export const products = [
     },
     {
         id: 6,
+        title: "Pantalon",
+        img: pontalon1,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    },
+    {
+        id: 7,
+        title: "Pantalon",
+        img: pantalon3,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    },
+    {
+        id: 8,
+        title: "Pull",
+        img: pull3,
+        category:"Femme",
+        type: "pull",
+        price: 60
+
+    },
+    {
+        id: 9,
+        title: "Pantalon",
+        img: pantalon4,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    },
+    {
+        id: 10,
+        title: "Pull",
+        img: pull3,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    },
+    {
+        id: 11,
+        title: "Pull",
+        img: pull4,
+        category:"Femme",
+        type: "pull",
+        price: 60
+
+    },
+    {
+        id: 11,
+        title: "Pantalon",
+        img: pontalon1,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    },
+    {
+        id: 12,
+        title: "Pull",
+        img: pull5,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    },
+    {
+        id: 13,
+        title: "Pull",
+        img: pull6,
+        category:"Femme",
+        type: "pull",
+        price: 60
+
+    },
+    {
+        id: 14,
+        title: "Pull",
+        img: pull8,
+        category:"Femme",
+        type: "pull",
+        price: 60
+
+    },
+    {
+        id: 15,
+        title: "Pull",
+        img: pull9,
+        category:"Femme",
+        type: "pull",
+        price: 60
+
+    }
+    /*{
+        id: 7,
         title: "Pantalon Gris",
         img: pontalon1,
         category:"Femme",
@@ -81,7 +204,25 @@ export const products = [
         type: "pantalon",
         price: 60
 
-    }
+    },
+    {
+        id: 7,
+        title: "Pantalon Gris",
+        img: pontalon1,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    },
+    {
+        id: 7,
+        title: "Pantalon Gris",
+        img: pontalon1,
+        category:"Femme",
+        type: "pantalon",
+        price: 60
+
+    }*/
 
     
 
